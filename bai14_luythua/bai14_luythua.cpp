008535e0e96fe41948fca88d421b5c0020e1596d
SPC <SPC@08-LAB402>
2025-01-03 10:31:57 +0700

    Add project files.

---
+++ bai14_luythua/bai14_luythua.cpp
@@ -0,0 +1,38 @@
+// bai14_luythua.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int tinhluythua(int s, int m)
+{
+    int lt = 1;
+    for (int i = 1; i <= m; i++)
+    {
+        lt *= s;
+    }
+    return lt;
+}
+int main()
+{
+    cout << "Tinh luy thua\n";
+    int so, somu, luythua = 1;
+   cout << "Nhap so : "; cin >> so;
+   cout << "Nhap so mu: "; cin >> somu;
+ 
+    for (int i = 1; i <= somu; i++)
+    {
+        luythua *= so;
+    }cout << "luy thua: " << luythua;
+    cout << "\nLuy thua: " << tinhluythua(so,somu);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
